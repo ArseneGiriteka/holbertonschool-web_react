7e396b12a20647aa7c6e51e656aec4254e5c5cff
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-09-27 10:53:39 +0200

    task 4 done

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -17,3 +17,26 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+//task 4
+
+interface StudentClassConsturctorInteface {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    constructor(public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName() {
+        return this.firstName;
+    }
+}
